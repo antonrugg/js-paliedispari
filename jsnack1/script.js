d5f4a33bd868b91e3c8d11d8c5c2e37cef981d24
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-14 16:29:08 +0100

    palindrome end

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,23 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+console.log('js ok');
+
+function checkIfPalindrome(word) {
+    const len = word.length;
+
+    for (let i = 0; i < len / 2; i++){
+        if (word[i] !== word[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+const word = prompt('Enter a word to check if it is a palindrome: ');
+
+const value = checkIfPalindrome(word);
+
+console.log(value);
+
